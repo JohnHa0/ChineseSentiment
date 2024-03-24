986d4f55b64b03d98b79d56a77526b6da999adce
Ha0 <ha0@hao.com>
2024-03-24 08:59:38 +0800

    优化release数据处理，解决中文读取错误； 剔除字典中舆情相关，仅仅保留专业领域词汇

--- scripts/release_data.py
+++ scripts/release_data.py
@@ -22,13 +22,13 @@ def prepare(obj):
 
 
 def pos_eva():
-    with open(os.path.join(DATA_DIR, 'pos_eva.txt')) as f:
+    with open(os.path.join(DATA_DIR, 'pos_eva.txt'), encoding='utf-8', errors='ignore') as f:
         words = set([x.strip() for x in f])
         return words
 
 
 def neg_eva():
-    with open(os.path.join(DATA_DIR, 'neg_eva.txt')) as f:
+    with open(os.path.join(DATA_DIR, 'neg_eva.txt'), encoding='utf-8', errors='ignore') as f:
         words = set([x.strip() for x in f])
         return words
 
@@ -36,35 +36,35 @@ def neg_eva():
 def release():
     with open(output_path, "wb") as output:
         result = {}
-        with open(os.path.join(DATA_DIR, 'pos.txt')) as f, \
-                open(MAINTAINED_POS) as maintained_pos:
+        with open(os.path.join(DATA_DIR, 'pos.txt'), encoding='utf-8', errors='ignore') as f, \
+                open(MAINTAINED_POS, encoding='utf-8', errors='ignore') as maintained_pos:
             pos_emotion = set([x.strip() for x in f]).union(
                 [x.strip() for x in maintained_pos])
 
             result["pos_emotion"] = pos_emotion
 
-        with open(os.path.join(DATA_DIR, 'neg.txt')) as f, \
-                open(MAINTAINED_NEG) as maintained_neg:
+        with open(os.path.join(DATA_DIR, 'neg.txt'),encoding='utf-8', errors='ignore') as f, \
+                open(MAINTAINED_NEG,encoding='utf-8', errors='ignore') as maintained_neg:
             neg_emotion = set([x.strip() for x in f]).union(
                 [x.strip() for x in maintained_neg])
 
             result["neg_emotion"] = neg_emotion
 
-        with open(os.path.join(DATA_DIR, 'pos_tc.txt')) as f:
+        with open(os.path.join(DATA_DIR, 'pos_tc.txt'), encoding='utf-8', errors='ignore') as f:
             pos_emotion = set([x.strip() for x in f])
 
             result["pos_emotion_tc"] = pos_emotion
 
-        with open(os.path.join(DATA_DIR, 'neg_tc.txt')) as f:
+        with open(os.path.join(DATA_DIR, 'neg_tc.txt'), encoding='utf-8', errors='ignore') as f:
             neg_emotion = set([x.strip() for x in f])
 
             result["neg_emotion_tc"] = neg_emotion
 
-        with open(os.path.join(DATA_DIR, 'pos_sentence.txt')) as f:
+        with open(os.path.join(DATA_DIR, 'pos_sentence.txt'), encoding='utf-8', errors='ignore') as f:
             pos_sentence = set([x.strip() for x in f])
             result["pos_sentence"] = pos_sentence
 
-        with open(os.path.join(DATA_DIR, 'neg_sentence.txt')) as f:
+        with open(os.path.join(DATA_DIR, 'neg_sentence.txt'), encoding='utf-8', errors='ignore') as f:
             neg_sentence = set([x.strip() for x in f])
 
             result["neg_sentence"] = neg_sentence
@@ -72,18 +72,19 @@ def release():
         result["pos_evaluation"] = pos_eva()
         result["neg_evaluation"] = neg_eva()
 
-        with open(os.path.join(DATA_DIR, "degrees.json")) as f:
+        with open(os.path.join(DATA_DIR, "degrees_copy.json"), encoding='utf-8', errors='ignore') as f:
             degrees = json.load(f)
             result["degrees"] = degrees
 
         negations = set(
-            json.load(open(os.path.join(DATA_DIR, 'negations.json'))))
+            json.load(open(os.path.join(DATA_DIR, 'negations.json'), encoding='utf-8', errors='ignore')))
 
         result["negations"] = negations
         # places = os.path.join(os.path.dirname(__file__), "../dictionaries/places.txt")
         # result["places"] = set(places)
 
         pickle.dump(result, output)
+        print("Dictionary Ready")
 
 
 if __name__ == "__main__":
